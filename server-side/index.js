fd08cca0822b8a78dfb9db4ac05c44819c5448c3
Shashank Bijapur <shashankcbijapur24@gmail.com>
2023-03-29 20:44:33 +0530

    completed

---
+++ server-side/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const {connection}=require("./config/db")
+const app=express();
+const { exerciseRoute } = require('./routes/exercise.route');
+app.use(express.json());
+
+
+
+
+app.use('/exercise', exerciseRoute);
+
+
+app.listen(4500, async(req,res)=>{
+    try{
+        await connection
+        console.log("Mongo is connected");
+
+    }catch(err){
+        console.log(`Mongo is not connected so check you console`);
+    }
+})
