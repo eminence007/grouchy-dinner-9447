ce76fb6590aaa6b4209172b7627a5c0666f3e32d
Ravi Rana <ravirana75036@gmail.com>
2023-03-30 20:29:41 +0530

    auth

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,11 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 import HomePage from "./Pages/HomePage";
+import SignUp from "./Pages/SignUp";
+import Login from "./Pages/Login";
+
 
 function App() {
   return (
     <div className="App">
       <HomePage />
+     
+    
     </div>
   );
 }
