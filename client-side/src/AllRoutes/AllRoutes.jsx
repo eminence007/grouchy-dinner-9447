ccd8a69692a43aa984fffd41174801014a3a38c3
Sunil Chaudhary <sunilchaudhary7789@gmail.com>
2023-04-01 01:32:24 +0530

    added sidebar client side

--- client-side/src/AllRoutes/AllRoutes.jsx
+++ client-side/src/AllRoutes/AllRoutes.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import { Route, Router, Routes } from "react-router-dom";
+import Sidebar from "../Components/Sidebar/Sidebar";
+import '../Components/Sidebar/Sidebar.css';
+import Dashboard from "./Dashboard"
+import Diary from "./Diary";
 const AllRoutes = () => {
-  return <div>AllRoutes</div>;
+  return <div>
+    <Sidebar>
+        <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/diary" element={<Diary />} />
+        </Routes>
+      </Sidebar>
+  </div>;
 };
 
 export default AllRoutes;
