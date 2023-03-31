9fb4d24cb9498fc54a4f3d3cb45805012c9add24
Shashank Bijapur <shashankcbijapur24@gmail.com>
2023-03-31 20:12:00 +0530

    complted

---
+++ server-side/model/diaryfood.model.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+const MacronutrientSchema = new mongoose.Schema({
+  protein: [{
+    name: { type: String },
+    weight: { type: Number },
+    availableIn100g: { type: Number }
+  }],
+  Fat: [{
+    name: { type: String },
+    weight: { type: Number },
+    availableIn100g: { type: Number }
+  }],
+  Carbohydrates: [{
+    name: { type: String },
+    weight: { type: Number },
+    availableIn100g: { type: Number }
+  }]
+});
+
+const NutrientSchema = new mongoose.Schema({
+  name: { type: String },
+  weight: { type: Number },
+  availableIn100g: { type: Number }
+});
+
+const FoodSchema = new mongoose.Schema({
+  foodName: { type: String },
+  Macronutrient: { type: MacronutrientSchema },
+  HighlightedNutrients: [{ type: NutrientSchema }],
+  Vitamins: [{ type: NutrientSchema }],
+  Minerals: [{ type: NutrientSchema }],
+  date: {
+    type: Date,
+    default: function() {
+      return new Date();
+    },
+    get: function(val) {
+      return `${val.getDate()}/${val.getMonth() + 1}/${val.getFullYear()}`;
+    }
+  }
+});
+
+const FoodDataSchema = new mongoose.Schema({
+  foodData: [{ type: FoodSchema }]
+},
+{
+    versionKey:false,
+}
+);
+
+const FoodModel = mongoose.model('FoodData', FoodDataSchema);
+
+module.exports = {FoodModel};
