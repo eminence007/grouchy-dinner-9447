61b853d380243dfcd3cf499c99d301dd7124d952
Shashank Bijapur <shashankcbijapur24@gmail.com>
2023-04-02 17:54:35 +0530

    working on dashboard

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -2,11 +2,14 @@ import "./App.css";
 import HomePage from "./Pages/HomePage";
 import SignUp from "./Pages/SignUp";
 import Login from "./Pages/Login";
-
+import Graph from "./Pages/dashboard"
+import MyComponent from "./Pages/progress";
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      {/* <HomePage /> */}
+      <Graph/>
+     <MyComponent/>
       {/* <SignUp /> */}
       {/* <Login /> */}
     </div>
